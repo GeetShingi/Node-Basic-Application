29f5b9235ff6dd05659d95da4ffa2ff2b4687d51
GeetShingi <geet.shingi@gmail.com>
2019-01-12 23:30:52 +0530

    Basic CRUD opearations on mongodb using node js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '336617707192453',
+        clientSecret: '76fc698a62d0474c8b92e750430f9eba'
+    }
+}
